15cd8c38ebb3ee5db6b50ba8df0488647f632be7
zilean <1215569310@qq.com>
2022-03-17 22:43:05 +0800

    Add project files.

---
+++ Shader.cpp
@@ -0,0 +1,156 @@
+#include "Shader.h"
+
+
+// constructor generates the shader on the fly
+// ------------------------------------------------------------------------
+Shader::Shader(const char* vertexPath, const char* fragmentPath, const char* geometryPath)
+{
+    // 1. retrieve the vertex/fragment source code from filePath
+    std::string vertexCode;
+    std::string fragmentCode;
+    std::string geometryCode;
+    std::ifstream vShaderFile;
+    std::ifstream fShaderFile;
+    std::ifstream gShaderFile;
+    // ensure ifstream objects can throw exceptions:
+    vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    gShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    try
+    {
+        // open files
+        vShaderFile.open(vertexPath);
+        fShaderFile.open(fragmentPath);
+        std::stringstream vShaderStream, fShaderStream;
+        // read file's buffer contents into streams
+        vShaderStream << vShaderFile.rdbuf();
+        fShaderStream << fShaderFile.rdbuf();
+        // close file handlers
+        vShaderFile.close();
+        fShaderFile.close();
+        // convert stream into string
+        vertexCode = vShaderStream.str();
+        fragmentCode = fShaderStream.str();
+        // if geometry shader path is present, also load a geometry shader
+        if (geometryPath != nullptr)
+        {
+            gShaderFile.open(geometryPath);
+            std::stringstream gShaderStream;
+            gShaderStream << gShaderFile.rdbuf();
+            gShaderFile.close();
+            geometryCode = gShaderStream.str();
+        }
+    }
+    catch (std::ifstream::failure& e)
+    {
+        std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ: " << e.what() << std::endl;
+    }
+    const char* vShaderCode = vertexCode.c_str();
+    const char* fShaderCode = fragmentCode.c_str();
+    // 2. compile shaders
+    unsigned int vertex, fragment;
+    // vertex shader
+    vertex = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex, 1, &vShaderCode, NULL);
+    glCompileShader(vertex);
+    checkCompileErrors(vertex, "VERTEX");
+    // fragment Shader
+    fragment = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment, 1, &fShaderCode, NULL);
+    glCompileShader(fragment);
+    checkCompileErrors(fragment, "FRAGMENT");
+    // if geometry shader is given, compile geometry shader
+    unsigned int geometry;
+    if (geometryPath != nullptr)
+    {
+        const char* gShaderCode = geometryCode.c_str();
+        geometry = glCreateShader(GL_GEOMETRY_SHADER);
+        glShaderSource(geometry, 1, &gShaderCode, NULL);
+        glCompileShader(geometry);
+        checkCompileErrors(geometry, "GEOMETRY");
+    }
+    // shader Program
+    ID = glCreateProgram();
+    glAttachShader(ID, vertex);
+    glAttachShader(ID, fragment);
+    if (geometryPath != nullptr)
+        glAttachShader(ID, geometry);
+    glLinkProgram(ID);
+    checkCompileErrors(ID, "PROGRAM");
+    // delete the shaders as they're linked into our program now and no longer necessery
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+    if (geometryPath != nullptr)
+        glDeleteShader(geometry);
+
+}
+
+
+Shader::~Shader()
+{
+}
+
+
+void Shader::use()
+{
+	glUseProgram(ID);
+}
+
+
+void Shader::checkCompileErrors(unsigned int ID, string type)
+{
+	GLint success;
+	GLchar infoLog[512];
+	if (type._Equal("PROGRAM"))
+	{
+		glGetProgramiv(ID, GL_LINK_STATUS, &success);
+		if (!success)
+		{
+			glGetProgramInfoLog(ID, 512, NULL, infoLog);
+			cout << "ERROR::SHADER::PROGRAM::LINK_FAILED\n" << endl;
+		}
+	}
+	else if (type._Equal("VERTEX") || type._Equal("FRAGMENT"))
+	{
+		glGetShaderiv(ID, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			glGetShaderInfoLog(ID, 512, NULL, infoLog);
+			cout << "ERROR::SHADER::" << type << "COMPILATION_FAILED\n" << infoLog << endl;
+		}
+	}
+	else
+		cout << "Unchecked Error!" << endl;
+}
+
+
+void Shader::setMat4(const string& name, const glm::mat4& mat)
+{
+	glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+
+void Shader::setVec3(const string& name, const float x, const float y, const float z)
+{
+	unsigned int uniformLocation = glGetUniformLocation(ID, name.c_str());
+	glUniform3f(uniformLocation, x, y, z);
+
+}
+
+void Shader::setVec3(const string& name, const glm::vec3& value)
+{
+	unsigned int uniformLocation = glGetUniformLocation(ID, name.c_str());
+	glUniform3fv(uniformLocation, 1, &value[0]);
+}
+
+void Shader::setFloat(const string& name, const float& value)
+{
+	unsigned int uniformLocation = glGetUniformLocation(ID, name.c_str());
+	glUniform1f(uniformLocation, value);
+}
+
+void Shader::setInt(const string& name, const int value)
+{
+	unsigned int uniformLocation = glGetUniformLocation(ID, name.c_str());
+	glUniform1i(uniformLocation, value);
+}
+
---
+++ Shader.h
@@ -0,0 +1,42 @@
+#pragma once
+
+
+#define GLEW_STATIC
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <cstring>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+using namespace std;
+
+
+class Shader
+{
+public:
+
+	unsigned int ID;
+	// constructor generates the shader on the fly
+	// ------------------------------------------------------------------------
+	Shader(const char* vertexPath, const char* fragmentPath, const char* geometryPath = nullptr);
+	~Shader();
+	void use();
+	void setMat4(const string& name, const glm::mat4& mat);
+	void setVec3(const string& name, const float x, const float y, const float z);
+	void setVec3(const string& name, const glm::vec3& value);
+	void setFloat(const string& name, const float &value);
+	void setInt(const string& name, const int value);
+private:
+	string vertexString;
+	string fragmentString;
+	const char* vertexSource;
+	const char* fragmentSource;
+	unsigned int vertexShader;
+	unsigned int fragmentShader;
+	void checkCompileErrors(unsigned int ID, string type);
+};
+
+
